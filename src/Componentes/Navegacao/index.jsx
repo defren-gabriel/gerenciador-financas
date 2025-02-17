55b9b028f0bb83ad63d0a3cf48ae6d860f8d6443
Gabriel Fernandes <defren.gabriel@gmail.com>
2025-02-17 10:42:35 -0300

    adição dos estilos do componente Navegação - faltando apenas os estilos de fonte e cor no final

--- src/Componentes/Navegacao/index.jsx
+++ src/Componentes/Navegacao/index.jsx
@@ -1,8 +1,17 @@
+import styles from "./Navegacao.module.css";
+
 const Navegacao = () => {
     return(
-        <>
-            Componente de navegação
-        </>
+        <nav className={styles.container}>
+            <div className={styles.projdev}>
+                <a className={styles.projdeva} href="https://github.com/defren-gabriel/gerenciador-financas" target="_blank">Página do Projeto</a>
+                <a className={styles.projdeva} href="https://github.com/defren-gabriel" target="_blank">Página do DeFrEn</a>
+            </div>
+            <div className={styles.links}>
+                <span className={styles.linkss}>Gerenciar Finanças</span>
+                <button className={styles.linksb}>Sair</button>
+            </div>
+        </nav>
     );
 }
 
