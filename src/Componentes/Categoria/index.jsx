94ac98cf1fb9234315b6d378757fb2c19a27e315
Gabriel Fernandes <defren.gabriel@gmail.com>
2025-02-18 11:33:45 -0300

    construção da interface do componente Categoria que lista as categorias completo

--- src/Componentes/Categoria/index.jsx
+++ src/Componentes/Categoria/index.jsx
@@ -6,23 +6,53 @@ const Categoria = ({idcategoria, nomecategoria, data, registros}) => {
     //configura os states
     const [idCat, setIdcat] = useState("");
     const [nomeCat, setNomecat] = useState("");
-    const [da, setDa] = useState("");
-    const [reg, setReg] = useState([]);
+    const [da, setDa] = useState("");   //data
+    const [reg, setReg] = useState([]); //lista de registros
+    const [total, setTotal] = useState("");
 
     //ao renderizar o componente os dados são passados do props para os states
     useState(()=>{
         setIdcat(idcategoria);
         setNomecat(nomecategoria);
         setDa(data);
-        setReg(registros);
+        //filtra os registros para cada categoria
+        const tempRegistros = registros.filter((prev)=>prev.idcategoria == idcategoria);
+        setReg(tempRegistros);
+        //calcula o total
+        const soma = tempRegistros.reduce((acumulador, item) => {
+            const valorAjustado = item.tipogasto === 0 ? -item.valor : item.valor;
+            return acumulador + valorAjustado;
+        }, 0);
+        setTotal(soma.toFixed(2));
     }, []);
 
     return(
         <section className={styles.container_categoria}>
             {/*titulo e data*/}
             <div className={styles.titulo1}>
-                <h1 className={styles.titulo1h1}>{nomeCat}</h1>
-                <span className={styles.titulo1s}>{da}</span>
+                <h1 className={styles.titulo1cat}>{nomeCat}</h1>
+                <span className={styles.titulo1valor}>Valor</span>
+                <span className={styles.titulo1data}>{da}</span>
+            </div>
+            {
+                reg.map((item)=>(
+                    <div 
+                        key={item.id}
+                        className={styles.itemlinha}
+                    >
+                        <span className={styles.itemlinha1}>{item.descricao}</span>
+                        {
+                            !item.tipogasto ? 
+                            <span className={styles.itemlinha2}>R$ - {item.valor.toFixed(2)}</span> :
+                            <span className={styles.itemlinha2}>R$ {item.valor.toFixed(2)}</span>
+                        }
+                        <span className={styles.itemlinha3}>{item.data}</span>
+                    </div>
+                ))
+            }
+            <div className={styles.total}>
+                <strong className={styles.totaltitulo}>Total:</strong>
+                <strong className={styles.totalvalor}>R$ {total}</strong>
             </div>
         </section>
     );
--- src/Paginas/Controle/index.jsx
+++ src/Paginas/Controle/index.jsx
@@ -11,10 +11,10 @@ const Controle = () => {
         {id: 2, nome: "Empréstimos", data: "05 de fevereiro de 2025", idusuario: 1}
     ]);
     const [registros, setRegistros] = useState([
-        {id: 1, descricao: "Compras do inicio do mes", tipogasto: 0, data: "03 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
-        {id: 2, descricao: "Ração pros cachorros", tipogasto: 0, data: "16 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
-        {id: 3, descricao: "Salário do mês", tipogasto: 1, data: "26 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
-        {id: 4, descricao: "Empréstimo Ramon", tipogasto: 0, data: "01 de fevereiro de 2025", idcategoria: 2, idusuario: 1}
+        {id: 1, descricao: "Compras do inicio do mes", tipogasto: 0, valor: 345.32, data: "03 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
+        {id: 2, descricao: "Ração pros cachorros", tipogasto: 0, valor: 121.0, data: "16 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
+        {id: 3, descricao: "Salário do mês", tipogasto: 1, valor: 3467.9, data: "26 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
+        {id: 4, descricao: "Empréstimo Ramon", tipogasto: 0, valor: 300, data: "01 de fevereiro de 2025", idcategoria: 2, idusuario: 1}
     ]);
 
     return(
@@ -25,7 +25,7 @@ const Controle = () => {
                 categorias.map((item)=>(
                     <Categoria
                         key={item.id} 
-                        idcategoria={item.id}
+                        idcategoria={item.id} //id da categoria
                         nomecategoria={item.nome}
                         data={item.data}
                         registros={registros}
