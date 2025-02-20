b887b1d3ff6a9a5e96b3cf6b96cff32f1141f61e
Gabriel Fernandes <defren.gabriel@gmail.com>
2025-02-20 14:40:29 -0300

    Registro de categoria completo

--- src/Paginas/Controle/index.jsx
+++ src/Paginas/Controle/index.jsx
@@ -1,20 +1,48 @@
 import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useAuth } from "../../Contextos/Auth";
+import { db } from "../../Firebase/config";
+
+import { 
+    collection, 
+    addDoc,
+    query, 
+    where, 
+    onSnapshot,
+    deleteDoc, 
+    doc,
+    serverTimestamp,
+    orderBy,
+    getDocs
+} from "firebase/firestore";
+
+import {format} from "date-fns";
 
 import Categoria from "../../Componentes/Categoria";
 
 import styles from "./Controle.module.css";
 
 const Controle = () => {
+    //verifica se o usuario esta conectado
+    const {user} = useAuth();
+    const navigate = useNavigate();
+    useEffect(()=>{
+        if(!user){
+            navigate("/");
+        }
+    }, []);
+
     //mantem as categorias e registros para cada categoria - dados temporarios
     const [categorias, setCategorias] = useState([
-        {id: 1, nome: "Pessoais - Fevereiro", data: "02 de fevereiro de 2025", idusuario: 1},
-        {id: 2, nome: "Empréstimos", data: "05 de fevereiro de 2025", idusuario: 1}
+        /*{id: 1, nome: "Pessoais - Fevereiro", data: "02 de fevereiro de 2025", idusuario: 1},
+        {id: 2, nome: "Empréstimos", data: "05 de fevereiro de 2025", idusuario: 1}*/
     ]);
     const [registros, setRegistros] = useState([
-        {id: 1, descricao: "Compras do inicio do mes", tipogasto: 0, valor: 345.32, data: "03 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
+        /*{id: 1, descricao: "Compras do inicio do mes", tipogasto: 0, valor: 345.32, data: "03 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
         {id: 2, descricao: "Ração pros cachorros", tipogasto: 0, valor: 121.0, data: "16 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
         {id: 3, descricao: "Salário do mês", tipogasto: 1, valor: 3467.9, data: "26 de fevereiro de 2025", idcategoria: 1, idusuario: 1},
-        {id: 4, descricao: "Empréstimo Ramon", tipogasto: 0, valor: 300, data: "01 de fevereiro de 2025", idcategoria: 2, idusuario: 1}
+        {id: 4, descricao: "Empréstimo Ramon", tipogasto: 0, valor: 300, data: "01 de fevereiro de 2025", idcategoria: 2, idusuario: 1}*/
     ]);
 
     //referente ao botão de nova categoria e seu formulario
@@ -37,8 +65,30 @@ const Controle = () => {
     const handleRegistroCategoria = (e) => {
         e.preventDefault();
 
-        console.log("Registrando nova categoria");
+        registraCategoria(nomecategoria);
     }
+    const registraCategoria = async (categoria) => {
+        if (!user) return; // Garante que o usuário está autenticado
+    
+        //prepara a data formatada
+        const dataAtual = new Date();
+        const dataFormatada = format(dataAtual, "dd/MM/yyyy");
+
+        try {
+          await addDoc(collection(db, "categorias"), {
+            usuario: user.uid, // ID do usuário autenticado  
+            nome: categoria,
+            data: dataFormatada,
+          });
+          setNomecategoria("");
+          setEstaRegistrando(!estaRegistrando);
+        } catch (error) {
+          console.error("Erro ao adicionar item:", error);
+        }
+    };
+
+    //lista as categorias todas
+
 
     return(
         <main className={styles.container}>
